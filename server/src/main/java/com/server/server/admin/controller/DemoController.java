79d189c40f5f543f2e4553fcb3855f55dbad96be
marcinlewinski <marcinlewinski71@gmail.com>
2024-01-10 21:50:01 +0100

    ADD: CORS configuration has been implemented

--- server/src/main/java/com/server/server/admin/controller/DemoController.java
+++ server/src/main/java/com/server/server/admin/controller/DemoController.java
@@ -1,13 +1,11 @@
 package com.server.server.admin.controller;
 
 import jakarta.annotation.security.RolesAllowed;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/")
-
+@CrossOrigin(origins = "*")
 public class DemoController {
 
     @GetMapping("/everyone-authenticated")
--- server/src/main/java/com/server/server/security/SecurityConfiguration.java
+++ server/src/main/java/com/server/server/security/SecurityConfiguration.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 
 @Configuration
@@ -26,6 +30,14 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(List.of("*"));
+                    configuration.setAllowedMethods(List.of("*"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    return configuration;
+                }))
                 .authorizeHttpRequests(authRequests -> authRequests
                         .anyRequest().authenticated())
 
